a492acae46a16bd631cc8c1376748869ec662411
wwjoon <gogownsduq@gmail.com>
2023-03-05 18:35:46 +0900

    feat: admin panel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 /*
@@ -23,7 +24,16 @@ const handleListen = () => console.log(`Listening on http://localhost:${port}`);
 
 // http server 위에 websocket server를 만들기 위함, 동일한 포트에서 2가지 처리
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 const publicRooms = () => {
   const {
