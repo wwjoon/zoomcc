163ae34d7f9062ec09b78a192777de51f954f5f2
wwjoon <gogownsduq@gmail.com>
2023-03-15 23:38:14 +0900

    feat: added video function

--- src/public/js/video.js
+++ src/public/js/video.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const face = document.getElementById("face");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let stream;
+let muted = false;
+let cameraOff = false;
+
+const getMedia = async () => {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+    face.srcObject = stream;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getMedia();
+
+const handleMuteClick = () => {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+};
+
+const handleCameraClick = () => {
+  if (!!cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+};
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
